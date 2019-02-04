5871e1f837f67447712a8747bfd52447d7617e3e
Shashank Panwar <shashank.panwar@Dr-Mac.local>
2019-02-04 12:59:22 +0530

    Finished out demo framework

---
+++ DemoRepo/DemoRepo/DemoRepo.h
@@ -0,0 +1,19 @@
+//
+//  DemoRepo.h
+//  DemoRepo
+//
+//  Created by Shashank Panwar on 04/02/19.
+//  Copyright © 2019 Shashank Panwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoRepo.
+FOUNDATION_EXPORT double DemoRepoVersionNumber;
+
+//! Project version string for DemoRepo.
+FOUNDATION_EXPORT const unsigned char DemoRepoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoRepo/PublicHeader.h>
+
+
---
+++ DemoRepo/DemoRepo/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  DemoRepo
+//
+//  Created by Shashank Panwar on 04/02/19.
+//  Copyright © 2019 Shashank Panwar. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){}
+    
+    public static func doSomething() -> String{
+        return "Do something relevent"
+    }
+    
+}
